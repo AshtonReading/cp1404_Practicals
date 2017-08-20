869e0b52c447e52979b382e6122619cbe0202613
AshtonReading <ashton.reading,91@gmail.com>
2017-08-20 12:25:39 +1000

    Prac 4

--- Practical 4/list_stuff.py
+++ Practical 4/list_stuff.py
@@ -10,3 +10,4 @@ print("The last number is {}" .format(numbers[4]))
 print("The smallest number is {}" .format(int(min(numbers))))
 print("The largest number is {}" .format(int(max(numbers))))
 print("The average of the numbers is {}" .format(sum(numbers) / len(numbers)))
+
---
+++ Practical 4/security_checker.py
@@ -0,0 +1,12 @@
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye',
+'swei45''BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState',
+'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input("Enter Username: ")
+
+if username in usernames:
+    print("Access Granted")
+else:
+    print("Access Denied")
+
+
